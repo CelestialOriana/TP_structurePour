1a282f18973ff9b2f9e0131c3ce91f8ccc32e436
emmanuel.a <emmanuel.a@lycee-de-beauvoir.education>
2024-10-08 15:00:24 +0200

    Affichage 10 nombres

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+int main() {
+    int nombreDépart;
+
+    // Demande à l'utilisateur d'entrer un nombre de départ
+    cout << "Veuillez entrer un nombre de depart : ";
+    cin >> nombreDépart;
+
+    // Affichage
+    for (int i = 1; i <= 10; i++) {
+        cout << nombreDépart + i << endl;
+    }
+
     return 0;
 }
+
