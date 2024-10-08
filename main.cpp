65f1c7d97fee68d7c5196acfcbac1ab3f49d621b
emmanuel.a <emmanuel.a@lycee-de-beauvoir.education>
2024-10-08 15:04:40 +0200

    Procedure  affichage10Nombres()

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 using namespace std;
 
+//procédure
+void affichage10Nombres(int nbDepart) {
+    // Affiche les dix nombres suivants
+    for (int i = 1; i <= 10; i++) {
+        cout << nbDepart + i << endl;
+    }
+}
+
 int main() {
-    int nombreDépart;
+    int nombreDepart;
 
     // Demande à l'utilisateur d'entrer un nombre de départ
     cout << "Veuillez entrer un nombre de depart : ";
-    cin >> nombreDépart;
+    cin >> nombreDepart;
 
-    // Affichage
-    for (int i = 1; i <= 10; i++) {
-        cout << nombreDépart + i << endl;
-    }
+    // Appel de la procédure
+    affichage10Nombres(nombreDepart);
 
     return 0;
 }
-
